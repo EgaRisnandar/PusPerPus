65e0dea23ede80bf9870c060283511295ef40087
EgaRisnandar <egarisnandarppl1@gmail.com>
2024-04-25 08:49:10 +0700

    Commit Pertama dan Terakhirt

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('container')
+    <h2 style="text-align: center">Tentang Kami</h2>
+    <h5 style="text-align: center">PusPerPus, Tanpa datang kepada kami, Anda bisa meminjam buku kapan pun dan dimana pun.</h5>
+@endsection
